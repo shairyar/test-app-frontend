73b814a97849e3c57302003981f5cb11a7d1a4b9
Shairyar Baig <shairyarbaig@gmail.com>
2020-05-10 19:44:34 +0500

    sign in and sign up components basic layout and routing set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/authentication/sign-in/sign-in.component';
+import { SignUpComponent } from './components/authentication/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
